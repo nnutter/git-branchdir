7b539463d4dafbbbd17a65e2b7209800c830388f
Nathan Nutter <iam@nnutter.com>
2012-06-17 16:32:49 -0500

    fix a couple issues with gcd
    - fix missing function, branchdir_basedir
    - fix repeating tab completion

--- gcd.sh
+++ gcd.sh
@@ -1,19 +1,25 @@
 gcd_complete() {
     COMPREPLY=()
-    local cur=${COMP_WORDS[COMP_CWORD]}
-    COMPREPLY=($(compgen -W "$(git config --get-all branchdir.branch)" -- $cur))
+    if [ ${#COMP_WORDS[@]} -eq 2 ]; then
+        local cur=${COMP_WORDS[COMP_CWORD]}
+        COMPREPLY=($(compgen -W "$(git config --get-all branchdir.branch)" -- $cur))
+    fi
 }
 complete -F gcd_complete gcd
 
-gcd_branchdir_path() {
+branchdir_basedir() {
+    git config branchdir.base
+}
+
+branchdir_path() {
     branchname="$1"
     echo "$(branchdir_basedir)-${branchname//\//-}"
 }
 
 gcd() {
     if [ $# -eq 0 ]; then
-        cd $(git config branchdir.base)
+        cd "$(branchdir_basedir)"
     else
-        cd "$(gcd_branchdir_path $1)"
+        cd "$(branchdir_path $1)"
     fi
 }
