f8f15f734d29199efdde62bdc32ad024ede0a753
Nathan Nutter <iam@nnutter.com>
2012-06-17 17:39:30 -0500

    add alias master for basedir

--- gcd.sh
+++ gcd.sh
@@ -2,7 +2,7 @@ gcd_complete() {
     COMPREPLY=()
     if [ ${#COMP_WORDS[@]} -eq 2 ]; then
         local cur=${COMP_WORDS[COMP_CWORD]}
-        COMPREPLY=($(compgen -W "$(git config --get-all branchdir.branch)" -- $cur))
+        COMPREPLY=($(compgen -W "master $(git config --get-all branchdir.branch)" -- $cur))
     fi
 }
 complete -F gcd_complete gcd
@@ -13,7 +13,11 @@ branchdir_basedir() {
 
 branchdir_path() {
     branchname="$1"
-    echo "$(branchdir_basedir)-${branchname//\//-}"
+    if [ "$branchname" == "master" ]; then
+        echo "$(branchdir_basedir)"
+    else
+        echo "$(branchdir_basedir)-${branchname//\//-}"
+    fi
 }
 
 gcd() {
